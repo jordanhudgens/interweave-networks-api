c4052be6e4f3d337cff4c290a74a1b77744a71c3
jordanhudgens <jordan@devcamp.com>
2018-05-24 19:06:27 -0500

    Built out initial specs for api client

--- spec/models/api_client_spec.rb
+++ spec/models/api_client_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ApiClient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:generated_public_key) { SecureRandom.base64.tr('+/=', 'Qrt') }
+  let(:generated_api_secret) { SecureRandom.base64.tr('+/=', 'Qrt') }
+
+  it 'can be created' do
+    api_client = ApiClient.create!(public_key: generated_public_key, api_secret: generated_api_secret)
+
+    expect(api_client).to be_valid
+  end
+
+  it '.is_valid?' do
+
+  end
 end
