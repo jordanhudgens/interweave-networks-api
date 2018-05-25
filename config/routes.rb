75204c703167c159032ccaa72ee7b3c406e83939
jordanhudgens <jordan@devcamp.com>
2018-05-24 22:27:32 -0500

    Added an index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :leads
   resources :companies
   resources :industries
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'companies#index'
 end
