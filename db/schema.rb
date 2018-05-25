48621f09455eb4fa652a82cfa325c6c9c85303dd
jordanhudgens <jordan@devcamp.com>
2018-05-25 15:57:15 -0500

    Added a url attr to the companies table and built out initial data in the seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_201728) do
+ActiveRecord::Schema.define(version: 2018_05_25_205517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_05_25_201728) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "industries", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Company.create!(
+  title:       'Open Devos',
+  description: 'A faith based platform for discovering and publishing daily devotionals.',
+  position:    1
+)
+
+Company.create!(
+  title:       'CronDose',
+  description: 'Daily Dev Guides - Daily Snippets for the Developer Life',
+  position:    2
+)
+
+Company.create!(
+  title:       'Edutechional',
+  description: 'Video tutorials for programming, math, and computer science students',
+  position:    3
+)
+
+Company.create!(
+  title:       'Toastability',
+  description: 'The easiest way to manage your restaurant\'s website, marketing, and day to day operations',
+  position:    4
+)
+
+Company.create!(
+  title:       'Deep Coding Thought',
+  description: 'Programming and Development question and answer dashboard',
+  position:    5
+)
+
+Company.create!(
+  title:       'DevTrunk',
+  description: 'Developer eLearning flash sale and bundle marketplace',
+  position:    6
+)
+
+Company.create!(
+  title:       'DashTrack',
+  description: 'Daily productivity journal',
+  position:    7
+)
+
+
+Company.create!(
+  title:       'Beach Front Deals',
+  description: 'Beachfront hotel, dining, and shopping deals',
+  position:    8
+)
+
+Company.create!(
+  title:       'Estate Lyfe',
+  description: 'Luxury home showcase',
+  position:    9
+)
+
+Company.create!(
+  title:       'Tinyhouse Exchange',
+  description: 'Tinyhouse sale and rental marketplace',
+  position:    10
+)
+
+Company.create!(
+  title:       'Farmers Marketplace Exchange',
+  description: 'Farmers market discovery system',
+  position:    11
+)
