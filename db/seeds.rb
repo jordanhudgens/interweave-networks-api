4aa02bea986cc8dfe06b5bd2616d23b520c25021
jordanhudgens <jordan@devcamp.com>
2018-05-25 16:00:28 -0500

    Added url data to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,66 +1,77 @@
 Company.create!(
   title:       'Open Devos',
   description: 'A faith based platform for discovering and publishing daily devotionals.',
-  position:    1
+  position:    1,
+  url:         'https://www.opendevos.com'
 )
 
 Company.create!(
   title:       'CronDose',
   description: 'Daily Dev Guides - Daily Snippets for the Developer Life',
-  position:    2
+  position:    2,
+  url:         'https://www.crondose.com'
 )
 
 Company.create!(
   title:       'Edutechional',
   description: 'Video tutorials for programming, math, and computer science students',
-  position:    3
+  position:    3,
+  url:         'https://www.edutechional.com'
 )
 
 Company.create!(
   title:       'Toastability',
   description: 'The easiest way to manage your restaurant\'s website, marketing, and day to day operations',
-  position:    4
+  position:    4,
+  url:         'https://www.toastability.com'
 )
 
 Company.create!(
   title:       'Deep Coding Thought',
   description: 'Programming and Development question and answer dashboard',
-  position:    5
+  position:    5,
+  url:         'https://www.deepcodingthought.com'
 )
 
 Company.create!(
   title:       'DevTrunk',
   description: 'Developer eLearning flash sale and bundle marketplace',
-  position:    6
+  position:    6,
+  url:         'https://www.devtrunk.com'
 )
 
 Company.create!(
   title:       'DashTrack',
   description: 'Daily productivity journal',
-  position:    7
+  position:    7,
+  url:         'https://www.dashtrack.com'
 )
 
 
 Company.create!(
   title:       'Beach Front Deals',
   description: 'Beachfront hotel, dining, and shopping deals',
-  position:    8
+  position:    8,
+  url:         'https://www.beachfrontdeals.com'
 )
 
 Company.create!(
   title:       'Estate Lyfe',
   description: 'Luxury home showcase',
-  position:    9
+  position:    9,
+  url:         'https://www.estatelyfe.com'
 )
 
 Company.create!(
   title:       'Tinyhouse Exchange',
   description: 'Tinyhouse sale and rental marketplace',
   position:    10
+  url:         'https://www.tinyhouse.exchange'
 )
 
 Company.create!(
-  title:       'Farmers Marketplace Exchange',
+  title:       'Farmers Marketplace Xchange',
   description: 'Farmers market discovery system',
-  position:    11
+  position:    11,
+  url:         'https://www.farmsmarketplacexchange.exchange'
 )
