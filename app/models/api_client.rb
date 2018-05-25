6edd5fe274e5aad6372d70cb07b1cbaf6d9e56c9
jordanhudgens <jordan@devcamp.com>
2018-05-24 19:21:23 -0500

    Built out a check to ensure a request is valid

--- app/models/api_client.rb
+++ app/models/api_client.rb
@@ -1,3 +1,9 @@
 class ApiClient < ApplicationRecord
   validates_presence_of :public_key, :api_secret
+
+  private
+
+    def self.is_valid?(public_key:, api_secret:)
+      ApiClient.where(public_key: public_key, api_secret: api_secret)
+    end
 end
--- spec/models/api_client_spec.rb
+++ spec/models/api_client_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.describe ApiClient, type: :model do
   let(:generated_public_key) { SecureRandom.base64.tr('+/=', 'Qrt') }
   let(:generated_api_secret) { SecureRandom.base64.tr('+/=', 'Qrt') }
+  let(:api_client) { ApiClient.create!(public_key: generated_public_key, api_secret: generated_api_secret) }
 
   it 'can be created' do
-    api_client = ApiClient.create!(public_key: generated_public_key, api_secret: generated_api_secret)
-
     expect(api_client).to be_valid
   end
 
   it '.is_valid?' do
-
+    expect(ApiClient.is_valid?(public_key: generated_public_key, api_secret: generated_api_secret)).to be_truthy
   end
 end
