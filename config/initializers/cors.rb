f877d1604fe07a0035528c54a5f2675863938862
jordanhudgens <jordan@devcamp.com>
2018-05-25 23:48:09 -0500

    Added new api elements

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://www.interweavenetworks.com'
+    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+  end
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+  allow do
+    origins 'http://www.interweavenetworks.com'
+    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+  end
 
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+  allow do
+    origins 'http://localhost:8080'
+    resource '*', headers: :any, methods: [:get, :post, :options], credentials: true
+  end
+end
